ccddc04b2d9e4c42af33ea6d92766dd559112043
simeon336 <simeon.m.yordanov.2019@elsys-bg.org>
2022-10-12 10:28:23 +0300

    First commit

---
+++ JavaIntoo/src/org/elsys_bg/java/intro/Main.java
@@ -0,0 +1,37 @@
+package org.elsys_bg.java.intro;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i = 0;
+        char c = 'c';
+        long l = 2l;
+        float f = 2.3f;
+        double d = 3.36d;
+        System.out.println(d);
+        boolean b = true;
+
+        Integer io = 2;
+        io = i;
+
+        Double dd;
+        Long ll;
+        Character cc;
+        Float ff;
+        Boolean bb = false;
+        if(b) {
+            //neshtosi
+        } else if(bb) {
+            //drugo neshto si
+        } else {
+            //bum
+        }
+
+        int arr[] = new int[2];
+        int arr2[] = {5, 6, 7, 8};
+        
+        for(int j = 0; j < arr.length; j++){
+            arr = new int[20];
+        }
+    }
+}
